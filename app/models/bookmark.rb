cb6025c624bc364cee1b0754724f1cacfce1b075
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2014-07-27 01:26:49 +0200

    Bookmark URLs are added a HTML before saving to database.

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,13 @@
 class Bookmark < ActiveRecord::Base
-  validates :url, presence: true
+  VALID_URL_PATTERN = /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z/
+  validates :url, presence: true,
+            format: { with: VALID_URL_PATTERN }
   validates :name, presence: true
   belongs_to :user
+
+  before_save {
+    if not self.url.start_with?("http://")
+      self.url = "http://" + self.url
+    end
+  }
 end
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe Bookmark do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @bookmark = Bookmark.new(name: "Example Bookmark", url: "http://www.wp.pl",
+                     description: "Description for this bookmark")
+  end
+
+  subject { @bookmark }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:url) }
+  it { should respond_to(:description) }
+  it { should be_valid }
+
+  describe "without a name" do
+    before { @bookmark.name = "" }
+    it { should be_invalid }
+  end
+
+  describe "bad url" do
+    before { @bookmark.url = "ble" }
+    it { should be_invalid }
+  end
+
+  describe "good url but without http" do
+    before { @bookmark.url = "www.google.com" }
+    it { should be_valid }
+  end
+
+  describe "when url format is valid" do
+    it "should be valid" do
+      urls = %w[www.google.com google.com http://www.railstutorial.org/book/modeling_users] # TODO - add more urls..., especially long ones with hashes, question marks etc.
+      urls.each do |valid_url|
+        @bookmark.url = valid_url
+        expect(@bookmark).to be_valid
+        @bookmark.save
+        expect(@bookmark.url).to start_with "http://"
+      end
+    end
+  end
+
 end
+
